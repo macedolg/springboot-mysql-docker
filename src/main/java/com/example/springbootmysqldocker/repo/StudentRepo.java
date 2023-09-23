9e63d688ce3e79144845577ee68b91f192ae3609
Lucas Gabriel Macêdo <lucasgabriel060405@gmail.com>
2023-09-23 14:47:55 -0300

    [create] repository student

---
+++ src/main/java/com/example/springbootmysqldocker/repo/StudentRepo.java
@@ -0,0 +1,8 @@
+package com.example.springbootmysqldocker.repo;
+
+import com.example.springbootmysqldocker.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepo extends JpaRepository<Student , Integer> {
+
+}
